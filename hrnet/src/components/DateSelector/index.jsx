20a22e6a3c2671cce5bb26e948be9f5d261567b9
emnificent <github.com/emnificent>
2023-03-25 00:51:27 +0100

    fix: show date picker on click and focus

--- hrnet/src/components/DateSelector/index.jsx
+++ hrnet/src/components/DateSelector/index.jsx
@@ -1,8 +1,12 @@
 function DateSelector({ name, label }) {
+  function showPicker(event) {
+    event.target.showPicker()
+  }
+
   return (
     <>
       <label htmlFor={name}>{label}</label>
-      <input name={name} id={name} type='date' />
+      <input name={name} id={name} type='date' onClick={showPicker} onFocus={showPicker} />
     </>
   )
 }
