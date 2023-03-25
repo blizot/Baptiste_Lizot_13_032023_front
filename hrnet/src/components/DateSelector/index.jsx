71d572721af74963eab563467c971665d39dc979
emnificent <github.com/emnificent>
2023-03-25 01:19:16 +0100

    fix: reverting last fix, was a convenience fix that caused issues

--- hrnet/src/components/DateSelector/index.jsx
+++ hrnet/src/components/DateSelector/index.jsx
@@ -1,12 +1,8 @@
 function DateSelector({ name, label }) {
-  function showPicker(event) {
-    event.target.showPicker()
-  }
-
   return (
     <>
       <label htmlFor={name}>{label}</label>
-      <input name={name} id={name} type='date' onClick={showPicker} onFocus={showPicker} />
+      <input name={name} id={name} type='date' />
     </>
   )
 }
